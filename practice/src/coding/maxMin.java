2e9ac98818b9f0d4d79c55d0873be79df1c2d448
mohit <saroyemohit@gmail.com>
2020-10-25 03:30:25 -0700

    weekly work

---
+++ practice/src/coding/maxMin.java
@@ -0,0 +1,17 @@
+package coding;
+
+public class maxMin {
+
+	public String max(int[] ar) {
+		return "";
+	}
+	
+	public String min(int[] ar) {
+		return "";
+	}
+	
+	public static void main(String [] args) {
+		
+		maxMin mm = new maxMin();
+	}
+}
--- practice/src/coding/reverse.java
+++ practice/src/coding/reverse.java
@@ -1,5 +1,29 @@
 package coding;
 
+import java.util.Arrays;
+
 public class reverse {
+	
+	public int[] rev(int[] ar) {
+
+		System.out.println("Before: "+Arrays.toString(ar));
+		int start=0, end=ar.length-1;
+		while (start < end) {
+			int temp=ar[start];
+			ar[start]=ar[end];
+			ar[end]=temp;
+			start++;
+			end--;
+		}
+		System.out.println("After:  "+Arrays.toString(ar));
+		return ar;
+		
+	}
+	
+	public static void main(String [] args) {
+		reverse r = new reverse();
+		r.rev(new int []{1,2,3,4,5,6,7,8,9,10,11});
+		
+	}
 
 }
