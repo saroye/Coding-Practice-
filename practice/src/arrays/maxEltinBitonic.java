5c5634031f3b6fb39aede5a7a73ad6fdb72d938f
mohit <saroyemohit@gmail.com>
2020-10-27 07:16:32 -0700

    weekly work

---
+++ practice/src/arrays/maxEltinBitonic.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class maxEltinBitonic {
+	
+	public int max(int[] ar) {
+		
+		int start=0, end=ar.length-1;
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+		maxEltinBitonic b = new maxEltinBitonic();
+		
+	}
+
+}
