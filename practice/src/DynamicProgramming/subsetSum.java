1f175d0a3b025ceb5bd7fc94a202f1d9c8c93b6e
mohit <saroyemohit@gmail.com>
2020-11-13 05:41:18 -0800

    week 4

--- practice/src/DynamicProgramming/subsetSum.java
+++ practice/src/DynamicProgramming/subsetSum.java
@@ -5,7 +5,7 @@ import java.util.*;
  * 
  * @author Saroye
  *
- *	given an array an int n
+ *	given an array and int n
  *	return if a subset in the given array
  *	has a sum of n value
  */
@@ -31,17 +31,18 @@ public class subsetSum {
 
 	public boolean subSetSum( int[] ar, int sum) {
 		boolean[][] t =new boolean[ar.length+1][sum+1];
-		for(int i=0; i<t.length; i++) {
-			t[i][0]=false;
+		
+		for(int i=0; i<t[0].length; i++) {
+			t[0][i]=false;
 		}
 		for(int j=0; j<t.length; j++) {
 			t[j][0]=true;
 		}
-		for(int i=1; i<=t.length; i++) {
-			for(int j=1; j<=t[i].length; j++) {
+		for(int i=1; i<t.length; i++) {
+			for(int j=1; j<t[i].length; j++) {
 
 				if(ar[i-1]<=j) {
-					t[i-1][j]= 
+					t[i][j]= 
 							(
 								t[i][j-ar[i-1]] 
 								||
@@ -57,15 +58,16 @@ public class subsetSum {
 			System.out.println(Arrays.toString(t[i]));
 		}
 
-		return t[ar.length+1][sum];
+		return t[ar.length][sum];
 	}
 
 	public static void main(String[] args) {
 		subsetSum s = new subsetSum();
-		int [] val = new int [] {2,3,4,5};
+		int [] val = new int [] {1,3,4,10};
 		int n=12;
-		System.out.println("sum: "+n+" Array: "+Arrays.toString(val));
+		System.out.println("Array: 	   "+Arrays.toString(val));
+		System.out.println("sum: 	   "+n);
 		System.out.println("Recursive: "+s.isSubsetSum(val,val.length, n));
-		s.subSetSum(val, 8);
+		System.out.println("Iterative: "+s.subSetSum(val, n));
 	}
 }
