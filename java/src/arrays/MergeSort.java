80123c6506ccf87be97fe52d81961db1788a386f
mohit <saroyemohit@gmail.com>
2020-12-03 14:00:04 -0800

    week8

--- java/src/arrays/MergeSort.java
+++ java/src/arrays/MergeSort.java
@@ -3,52 +3,66 @@ package arrays;
 import java.util.Arrays;
 
 public class MergeSort {
-	
-	
-	public void merge(int A[ ] , int start, int mid, int end) {
-		
-		int first = start , center= mid+1;
 
-		int[] Arr =new int[end-start+1];
-		int k=0;
 
-		for(int i = start ;i <= end ;i++) {
-		    if(first > mid)  {   
-		       Arr[ k++] = A[ center++] ;
-		    }
+	void merge(int arr[], int start, int mid, int end) {  
 
-		   else if ( center > end)  { 
-		       Arr[k++] = A[first++];
-		   }
+		int l = mid - start + 1;  
+		int r = end - mid;  
 
-		   else if( A[first] < A[center])  {   
-		      Arr[k++] = A[first++];
-		   }
-		   else {
-		      Arr[k++] = A[center++]; 
-		   }
-		 }
-		  for (int r=0 ; r< k ; r++) {
-		     A[ start++] = Arr[r] ;                          
-		  }
+		int LeftArray[] = new int [l];  
+		int RightArray[] = new int [r];  
+
+		for (int i=0; i<l; ++i)  {
+			LeftArray[i] = arr[start + i];  
 		}
-	
-	public void merge_sort (int A[ ] , int start , int end )
-	   {
-	           if(start<end) {
-	           int mid = (start+end)/2;        
-	           merge_sort (A,start,mid);                
-	           merge_sort (A,mid+1,end);              
+		for (int j=0; j<r; ++j) {
+			RightArray[j] = arr[mid + 1+ j];  
+		}
+		
+		int i = 0, j = 0;  
+		int k = start;  
+		while ( i<l && j<r)  {  
+			if (LeftArray[i] <= RightArray[j])  {  
+				arr[k] = LeftArray[i];  
+				i++;  
+			}  
+			else {  
+				arr[k] = RightArray[j];  
+				j++;  
+			}  
+			k++;  
+		}  
+		while (i<l)  {  
+			arr[k] = LeftArray[i];  
+			i++;  
+			k++;  
+		}  
 
-	          merge(A,start,mid,end );   
-	   }                    
-	}
-	
-	public static void main(String[] args) {
-		MergeSort m = new MergeSort();
-		int[] ar= {10, 7, 8, 9, 1, 5,10,4,20,14,25,12,18,16,30,22,55,66,44,33}; 
-		m.merge_sort(ar,0, ar.length-1);
-		System.out.println(Arrays.toString(ar));
+		while (j<r)  {  
+			arr[k] = RightArray[j];  
+			j++;  
+			k++;  
+		}  
+	}  
+
+	void sort(int arr[], int start, int end)  {
+		if (start<end)  {  
+			int  mid= start+ (end-start)/2;
+			sort(arr, start, mid);  
+			sort(arr , mid+1, end);  
+			merge(arr, start, mid, end); 
+			//System.out.println("Array: "+Arrays.toString(arr));
+			
+		}
 	}
 
-}
+		public static void main(String[] args) {
+			MergeSort m = new MergeSort();
+			int[] ar= {11, 7, 8, 9, 1,5,10,2,4,20,3,14,25,6,12,18,16,15,30,17,13,22,55,66,44,19,33}; 
+			System.out.println("Before: "+Arrays.toString(ar));
+			m.sort(ar,0, ar.length-1);
+			System.out.println("After:  "+Arrays.toString(ar));
+		}
+
+	}
