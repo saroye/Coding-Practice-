22475c0062a6bc7e02fc2044c4fd5b14532846f3
mohit <saroyemohit@gmail.com>
2020-12-05 23:42:46 -0800

    week8

--- java/src/arrays/sum3.java
+++ java/src/arrays/sum3.java
@@ -4,40 +4,47 @@ import java.util.*;
 
 public class sum3 {
 
-	public ArrayList<ArrayList<Integer>> threeSum(int[] nums) {
-		ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> list = new ArrayList<List<Integer>>();
 		Arrays.sort(nums);
 		System.out.println(Arrays.toString(nums));
-		int start=0,end=nums.length-1;
-
-		while(start<end) {
-			int first=nums[start];
-			int second=nums[start+1];
-			int last=nums[end];
-			if(first+second+last==0) {
-				ArrayList<Integer> list_inner=new ArrayList<Integer>();
-				list_inner.add(first);
-				list_inner.add(second);
-				list_inner.add(last);
-				list.add(list_inner);
-				start=start+2;
-				last--;
-			}
-			else {
-				start=start+2;
-				last--;
+		int s=0,e=nums.length-1;
+		if (nums.length>=3) {
+			while(s<e) {
+				int first=nums[s];
+				int second=nums[s+1];
+				int last=nums[e];
+				if(first+second+last==0) {
+					ArrayList<Integer> list_inner=new ArrayList<Integer>();
+					list_inner.add(first);
+					list_inner.add(second);
+					list_inner.add(last);
+					list.add(list_inner);
+					s++;
+					e--;
+				}
+				else if((first+second) <last) {
+					s++;
+				}
+				else {
+					e--;
+				}
 			}
 		}
-		System.out.println("???");
+		else {
+			return list;
+		}
+		
+		System.out.println("------------");
 		for(int i=0; i<list.size(); i++) {
-			System.out.println(list.get(i));
+			System.out.println("List "+i+": "+list.get(i));
 		}
 		return list;
 	}
 
 	public static void main(String[] args) {
 		sum3 s = new sum3();
-		int[] ar = {-1,0,1,2,-1,-4};
+		int[] ar = {0,0,0,0};
 		
 		s.threeSum(ar);
 	}
